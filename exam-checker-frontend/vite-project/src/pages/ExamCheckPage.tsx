291717698d0c9c7dd0fa2f12799260e2dd2af8d7
GiliH <88968156+Gili-H@users.noreply.github.com>
2025-08-24 17:00:12 +0300

    Update ExamCheckPage.tsx

--- exam-checker-frontend/vite-project/src/pages/ExamCheckPage.tsx
+++ exam-checker-frontend/vite-project/src/pages/ExamCheckPage.tsx
@@ -5,17 +5,14 @@ import ExamCheckForm from "../components/ExamCheck/ExamCheckForm";
 import ExamPreview from "../components/ExamCheck/ExamPreview";
 
 const ExamCheckPage: React.FC = () => {
-    const [selectedFile, setSelectedFile] = useState<File | null>(null);
+    const [selectedFile] = useState<File | null>(null);
     
     return (
         <Box sx={{ p: 4 }}>
             <ExamCheckTitle />
             <Box sx={{ display: "flex", flexDirection: "row-reverse", gap: 4, mt: 4 }}>
                 <ExamPreview file={selectedFile} />
-                <ExamCheckForm
-                    onFileChange={setSelectedFile}
-                    selectedFile={selectedFile}
-                />
+                <ExamCheckForm />
             </Box>
         </Box>
     );
